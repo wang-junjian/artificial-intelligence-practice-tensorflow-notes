75adfb0077eb26f808479341edc5618df4dd02c7
wang-junjian <wang-junjian@qq.com>
2018-09-06 16:51:55 +0800

    编写第4.1课 损失函数的源码

---
+++ source_code/opt4_1.py
@@ -0,0 +1,32 @@
+import numpy as np
+import tensorflow as tf
+
+BATCH_SIZE = 8
+SEED = 23455
+
+rdm = np.random.RandomState(SEED)
+
+X = rdm.rand(32, 2)
+Y_ = [[x0+x1+(rdm.rand()/10.0-0.05)] for (x0,x1) in X]
+
+x = tf.placeholder(tf.float32, shape=(None, 2))
+y_ = tf.placeholder(tf.float32, shape=(None, 1))
+w1 = tf.Variable(tf.random_normal((2,1), stddev=1, seed=1))
+
+y = tf.matmul(x, w1)
+
+loss = tf.reduce_mean(tf.square(y-y_))
+train_step = tf.train.GradientDescentOptimizer(0.001).minimize(loss)
+
+with tf.Session() as sess:
+	init_op = tf.global_variables_initializer()
+	sess.run(init_op)
+
+	STEPS = 20000
+	for i in range(STEPS):
+		start = i*BATCH_SIZE % 32
+		end = start+BATCH_SIZE
+		sess.run(train_step, feed_dict={x: X[start:end], y_: Y_[start:end]})
+		if (i+1) % 500 == 0:
+			print(i, ' w1: ', sess.run(w1))
+
